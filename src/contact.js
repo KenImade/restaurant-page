16c003fe3a5b1da72b56bff76e11c88d934d10a3
Kenneth Imade <kenneth.imade@yahoo.com>
2022-03-18 12:36:36 +0100

    added contact page bg picture

--- src/contact.js
+++ src/contact.js
@@ -1,6 +1,19 @@
+function createContactTitle() {
+    const contactHeading = document.createElement("div");
+    contactHeading.classList.add("contact-heading");
+    const contactTitle = document.createElement("p");
+    contactTitle.textContent = "Contact Us";
+
+    contactHeading.appendChild(contactTitle);
+    return contactHeading;
+};
+
+
 function createContactPage() {
     const contact = document.createElement("div");
     contact.classList.add("contact");
+
+    contact.appendChild(createContactTitle());
 
     return contact;
 };
